a1fe6a4cefad9cd9e24c874eb7dca32e2985428a
Ashkan <ashkan.bahar@gmail.com>
2026-09-14 12:21:17 +0200

    Clean-ups

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const h1Changer = document.querySelector("#h1Changer");
-const dynamicH1 = document.querySelector(".dynamicH1");
+const dynamicH1 = document.querySelector("#dynamicH1");
 h1Changer.addEventListener(
     "click",
     function(){ 
-        dynamicH1.textContent = "Goodbye World";
+        dynamicH1.textContent = "Goodbye World"; //Jag antar att "textContent" funkar bättre än "innerHTML" i det här fallet?
     }
 );
